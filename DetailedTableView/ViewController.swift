81211d5a29f84248890c3056fba5c4d63c4f21a2
ogullcankara <ogullcankara@gmail.com>
2022-09-01 00:40:58 +0300

    Initial Commit

---
+++ DetailedTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailedTableView
+//
+//  Created by Ogulcan Kara on 1.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
